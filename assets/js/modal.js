055cdbe7a6e6916b15d9ab9e73e3071bb7535228
Peng Li <pli9@bwh.harvard.edu>
2026-03-24 08:47:27 -0400

    Site updated at 2026-03-24 08:47:27 -0400

---
+++ assets/js/modal.js
@@ -0,0 +1,45 @@
+// // create references to the modal...
+// var modal = document.getElementById('myModal');
+// // to all images -- note I'm using a class!
+// var images = document.getElementsByClassName('myImages');
+// // the image in the modal
+// var modalImg = document.getElementById("img01");
+// // and the caption in the modal
+// var captionText = document.getElementById("caption");
+// 
+// // Go through all of the images with our custom class
+// for (var i = 0; i < images.length; i++) {
+// var img = images[i];
+// // and attach our click listener for this image.
+// img.onclick = function(evt) {
+// modal.style.display = "block";
+// modalImg.src = this.src;
+// captionText.innerHTML = this.alt;
+// }
+// }
+// 
+// var span = document.getElementsByClassName("close")[0];
+// 
+// span.onclick = function() {
+// modal.style.display = "none";
+// }
+
+// Fixed Modal Logic
+const modal = document.getElementById("myModal");
+const modalImg = document.getElementById("img01");
+
+function openModal(element) {
+    modal.style.display = "block";
+    modalImg.src = element.src;
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+// Close modal when clicking outside the image
+window.onclick = function(event) {
+    if (event.target == modal) {
+        closeModal();
+    }
+}
